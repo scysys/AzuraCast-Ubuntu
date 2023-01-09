d9a177057a1da67fa5800663cea94a67bd010e66
scysys <scysys@outlook.com>
2023-01-09 10:47:00 +0100

    fix

--- misc/finalize.sh
+++ misc/finalize.sh
@@ -10,6 +10,5 @@ apt-get update -o DPkg::Lock::Timeout=-1
 apt-get upgrade -o DPkg::Lock::Timeout=-1 -y
 
 # Create a file where i can get the installer version
-touch /var/azuracast/www/installer_version_$set_installer_version.txt
 echo "do not delete" > /var/azuracast/www/installer_version_$set_installer_version.txt
 chown azuracast.azuracast /var/azuracast/www/installer_version_$set_installer_version.txt
