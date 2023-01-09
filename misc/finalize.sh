f3e01337ac9b34d0e5b2400ae425d61acfc69673
scysys <scysys@outlook.com>
2023-01-09 09:50:33 +0100

    prepare for future updater

--- misc/finalize.sh
+++ misc/finalize.sh
@@ -4,5 +4,11 @@
 # finalize
 ##############################################################################
 
+# Check if everything is right
 apt-get update -o DPkg::Lock::Timeout=-1
 apt-get upgrade -o DPkg::Lock::Timeout=-1 -y
+
+# Create a file where i can get the installer version
+touch /var/azuracast/www/installer_version_$set_installer_version.txt
+echo "do not delete" > /var/azuracast/www/installer_version_$set_installer_version.txt
+chown azuracast.azuracast /var/azuracast/www/installer_version_$set_installer_version.txt
