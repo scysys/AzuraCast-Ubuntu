002f048c65750a0718f706ea04a937a72b9530bc
scysys <scysys@outlook.com>
2023-02-21 16:00:51 +0100

    Update finalize.sh

--- misc/finalize.sh
+++ misc/finalize.sh
@@ -7,6 +7,9 @@ apt-get install -o DPkg::Lock::Timeout=-1 -yf
 # Upgrade all packages and dependencies
 apt-get upgrade -o DPkg::Lock::Timeout=-1 -y
 
+# Remove not needed packages
+apt-get autoremove -o DPkg::Lock::Timeout=-1 -y
+
 # Create a file to track the installer version
 echo "do not delete" > "/var/azuracast/www/installer_version_$set_installer_version.txt"
 chown azuracast:azuracast "/var/azuracast/www/installer_version_$set_installer_version.txt"
