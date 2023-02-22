e33a2b357158c394372a03e6f905018f4a15f0a8
scysys <scysys@outlook.com>
2023-02-22 14:00:43 +0100

    Update centrifugo.sh

--- web/setup/centrifugo.sh
+++ web/setup/centrifugo.sh
@@ -1,34 +1,16 @@
 #!/usr/bin/env bash
 
-# Set variables for the golang version and centrifugo URL
-GOLANG_VERSION=1.18
-CENTRIFUGO_URL=https://github.com/centrifugal/centrifugo/archive/d465b5932ab786273f081392e1dc8fdfd2d2ec10.tar.gz
+# Install the Golang package without recommended packages
+apt-get install -y --no-install-recommends golang
 
-# Update the package lists and install the necessary dependencies
-apt-get update
-apt-get install -y curl tar gzip ca-certificates
+# Install a specific version of Centrifugo using Go
+go install github.com/centrifugal/centrifugo/v4@d465b5932ab786273f081392e1dc8fdfd2d2ec10
 
-# Download and install golang
-curl -fsSL https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz | tar -C /usr/local -xz
+# Move the centrifugo binary to /usr/local/bin
+mv /root/go/bin/centrifugo /usr/local/bin/centrifugo
 
-# Set the necessary environment variables for golang
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=$HOME/go
+# Remove the Go installation directory
+rm -rf /root/go
 
-# Download and extract the centrifugo source code
-curl -fsSL $CENTRIFUGO_URL | tar -C /usr/local -xz
-mv /usr/local/centrifugo* /usr/local/centrifugo
-
-# Build and install centrifugo
-cd /usr/local/centrifugo
-go build -o /usr/local/bin/centrifugo .
-
-# Copy the centrifugo configuration file to the AzuraCast directory
-cp $installerHome/web/centrifugo/config.json /var/azuracast/centrifugo/
-
-# Clean up by removing the go directory
-rm -rf $GOPATH
-rm -rf /usr/local/go
-
-# Go back to the installer home directory
-cd $installerHome
+# Copy the Centrifugo configuration file to the appropriate directory
+cp web/centrifugo/config.json /var/azuracast/centrifugo/config.json
