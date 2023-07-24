d8c431a73515fdbecfc24bfd0f1a6f920d86934c
scysys <scysys@outlook.com>
2023-07-24 22:35:46 +0200

    #16 attempt 1

--- stations/setup/liquidsoap.sh
+++ stations/setup/liquidsoap.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Install packages required by Liquidsoap and optional audio plugins
-apt-get update
-apt-get install -y --no-install-recommends \
-  libao-dev libasound2-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev \
-  libavutil-dev libfaad-dev libfdk-aac-dev libflac-dev libfreetype-dev libgd-dev libjack-dev \
-  libjpeg-dev liblo-dev libmad0-dev libmagic-dev libmp3lame-dev libopus-dev libpng-dev \
-  libportaudio2 libpulse-dev libsamplerate0-dev libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev \
-  libshine-dev libsoundtouch-dev libspeex-dev libsrt-openssl-dev libswresample-dev \
-  libswscale-dev libtag1-dev libtheora-dev libtiff-dev libx11-dev libxpm-dev bubblewrap ffmpeg \
-  frei0r-plugins-dev ladspa-sdk multimedia-audio-plugins swh-plugins tap-plugins lsp-plugins-ladspa
+apt_get_with_lock update
+apt_get_with_lock install -y --no-install-recommends \
+    libao-dev libasound2-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev \
+    libavutil-dev libfaad-dev libfdk-aac-dev libflac-dev libfreetype-dev libgd-dev libjack-dev \
+    libjpeg-dev liblo-dev libmad0-dev libmagic-dev libmp3lame-dev libopus-dev libpng-dev \
+    libportaudio2 libpulse-dev libsamplerate0-dev libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev \
+    libshine-dev libsoundtouch-dev libspeex-dev libsrt-openssl-dev libswresample-dev \
+    libswscale-dev libtag1-dev libtheora-dev libtiff-dev libx11-dev libxpm-dev bubblewrap ffmpeg \
+    frei0r-plugins-dev ladspa-sdk multimedia-audio-plugins swh-plugins tap-plugins lsp-plugins-ladspa
 
 # Download and install Liquidsoap
 ARCHITECTURE=$(dpkg --print-architecture | awk -F- '{ print $NF }')
@@ -17,7 +17,7 @@ wget -O liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/
 dpkg -i liquidsoap.deb
 
 # Install any missing dependencies
-apt-get install -y -f --no-install-recommends
+apt_get_with_lock install -y -f --no-install-recommends
 
 # Create a symbolic link for liquidsoap
 ln -s /usr/bin/liquidsoap /usr/local/bin/liquidsoap
