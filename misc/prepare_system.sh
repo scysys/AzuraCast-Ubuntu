668a3de683ac5b5ba3f7bf96145d1c7a5bf0ae30
scysys <scysys@outlook.com>
2023-07-25 17:17:29 +0200

    prevent installer from running azuracast installation twice

--- misc/prepare_system.sh
+++ misc/prepare_system.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Installer already runned / EXIT
+if [ -e "$installerHome/azuracast_installer_runned" ]; then
+  echo "Installer has already been run. Exiting..."
+  exit 1
+fi
+
 # Update package lists and upgrade packages
 apt_get_with_lock update
 apt_get_with_lock upgrade -y
@@ -12,19 +18,22 @@ add-apt-repository -y restricted
 # Update package lists
 apt_get_with_lock update
 
+# Prevent Installer to run more than one time
+touch $installerHome/azuracast_installer_runned
+
 # Issue: https://github.com/scysys/AzuraCast-Ubuntu/issues/1#issuecomment-1440983104
 # Fix RackNerds Image
 if grep -q "^adm:" /etc/group; then
   echo "adm group already exists, nothing to do."
 else
   echo "adm group not found. Adding adm group with members syslog and ubuntu."
-  echo "adm:x:4:syslog,ubuntu" >> /etc/group
+  echo "adm:x:4:syslog,ubuntu" >>/etc/group
 fi
 
 # Install system packages and dependencies
 apt_get_with_lock install -y build-essential pwgen whois zstd software-properties-common \
-    apt-transport-https ca-certificates language-pack-en tini gosu curl wget \
-    tar zip unzip git rsync tzdata gpg-agent openssh-client openssl
+  apt-transport-https ca-certificates language-pack-en tini gosu curl wget \
+  tar zip unzip git rsync tzdata gpg-agent openssh-client openssl
 
 # Set the system locale to en_US.UTF-8
 # TODO: Not sure why it's here. It's from AzuraCast's default repo. Maybe some checks need the English language, so it's better to leave it for now.
