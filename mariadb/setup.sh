d3c553bc0b03b076d8815d2213339181010c3cf7
Sven <scysys@outlook.com>
2026-01-08 14:10:31 +0100

    internal fix for my internal version

--- mariadb/setup.sh
+++ mariadb/setup.sh
@@ -4,36 +4,41 @@
 # setup_mariadb
 ##############################################################################
 
-# It seems that Azuracat is actually need MariaDB 10.9? Actual default Ubuntu version is 10.6
+# It seems that Azuracast actually needs MariaDB 10.9? Actual default Ubuntu version is 10.6
 apt_get_with_lock install -y wget software-properties-common dirmngr ca-certificates apt-transport-https
 
-if [ $azuracast_git_version = "stable" ]; then
-    curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-$set_mariadb_version"
+if [ "$azuracast_git_version" = "stable" ]; then
+    curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup \
+    | sudo bash -s -- --mariadb-server-version="mariadb-$set_mariadb_version"
 else
-    apt_get_with_lock install software-properties-common gnupg2 -y
-    apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-
-    echo "# MariaDB Server
+    apt_get_with_lock install -y software-properties-common gnupg2 curl
+    
+    mkdir -p /etc/apt/keyrings
+    
+    curl -fsSL https://mariadb.org/mariadb_release_signing_key.asc \
+    | gpg --dearmor | tee /etc/apt/keyrings/mariadb.gpg >/dev/null
+    
+    # Legacy fallback for apt-key (required for MaxScale repo)
+    apt-key add /etc/apt/keyrings/mariadb.gpg
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys \
+    5D87FACA8C27D14E CE1A3DD5E3C94F49
+    
+    cat <<'EOF' | sudo tee /etc/apt/sources.list.d/mariadb.list
+# MariaDB Server
 # To use a different major version of the server, or to pin to a specific minor version, change URI below.
 deb [arch=amd64,arm64] https://dlm.mariadb.com/repo/mariadb-server/10.8/repo/ubuntu jammy main
-
 deb [arch=amd64,arm64] https://dlm.mariadb.com/repo/mariadb-server/10.8/repo/ubuntu jammy main/debug
-
 
 # MariaDB MaxScale
 # To use the latest stable release of MaxScale, use "latest" as the version
 # To use the latest beta (or stable if no current beta) release of MaxScale, use "beta" as the version
 deb [arch=amd64,arm64] https://dlm.mariadb.com/repo/maxscale/latest/apt jammy main
 
-
 # MariaDB Tools
-deb [arch=amd64] http://downloads.mariadb.com/Tools/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/mariadb.list
-
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CE1A3DD5E3C94F49
-    sudo cp /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d
+deb [arch=amd64] http://downloads.mariadb.com/Tools/ubuntu jammy main
+EOF
     
     apt_get_with_lock update -y
-
 fi
 
 apt_get_with_lock install -y mariadb-server mariadb-client
@@ -44,15 +49,15 @@ mysql -e "create user $set_azuracast_username@localhost identified by '$set_azur
 mysql -e "grant all privileges on $set_azuracast_database.* to $set_azuracast_username@localhost;"
 
 # Prepare MySQL-Root-Password
-if [ $azuracast_git_version = "stable" ]; then
+if [ "$azuracast_git_version" = "stable" ]; then
     sed -i "s/changeToMySQLRootPW/$mysql_root_pass/g" mariadb/config/mysql_secure_installation.sql
-
+    
     # Secure MySQL in same way like: mysql_secure_installation
     mysql -sfu root <"mariadb/config/mysql_secure_installation.sql"
 else
     echo "do nothing, will do it later in another way"
 fi
 
-# Because of AzuraCasts Supervisor Integration
+# Because of AzuraCast Supervisor Integration
 systemctl disable mariadb
 systemctl stop mariadb
